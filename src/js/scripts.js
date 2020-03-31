91380183d5be378e01a656498808dcd57482a748
Molchanova Tatyana <mtuspeh@gmail.com>
2020-03-31 08:27:41 +0300

    n commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,20 @@
 $(document).ready(function(){
 	'use strict';
 
-	
+	document.getElementById('bar-btn').addEventListener('click', function() {
 
+		let width = 1;
 
+		var progress = setInterval(bar, 10);
 
-
+		function bar() {
+			if (width>= 100) {
+				clearInterval(progress);
+			} else {
+				width++;
+				document.getElementById('bar').style.width = width + "%";
+				document.getElementById('percent').textContent = width + "%";
+			}
+		}
+	});
 });
